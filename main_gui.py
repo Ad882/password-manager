647b636122a4e5e9533c7fb623b8dabde32ae0e2
Adrien <111064150+Ad882@users.noreply.github.com>
2024-12-28 14:23:59 +0100

    Handling the initialization :bug:

--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,22 @@
-from ui.main_window import PasswordManagerApp
+import os
+import getpass
 import tkinter as tk
+from ui.main_window import PasswordManagerApp
+from security import Security
+from database.database import Database
 
 def main():
+    master_password_file = "master_password.txt"
+    db_path = "database/passwords.db"
+
+    if not os.path.exists(master_password_file):
+        if os.path.exists(db_path):
+            os.remove(db_path)
+        db = Database(db_path)
+        security = Security(db=db)
+        master_password = getpass.getpass("Entrez un nouveau mot de passe maître: ")
+        security.initialize_master_password(master_password)
+    
     root = tk.Tk()
     app = PasswordManagerApp(root)
     root.mainloop()
